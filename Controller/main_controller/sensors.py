d207670a924ee46e06fcd5e343806a711fea697e
Jenjira (Jen) <164031660+Jkong26@users.noreply.github.com>
2026-04-17 23:31:38 +1000

    Update sensors.py

--- Controller/main_controller/sensors.py
+++ Controller/main_controller/sensors.py
@@ -1,1 +1,19 @@
+class Sensors:
+    def __init__(self, robot, ps):
+        self.robot = robot
+        self.ps = ps
 
+    def read(self):
+        values = [p.getValue() for p in self.ps]
+
+        # ps0 = front-right, ps7 = front-left → average for front
+        # ps1 = right, ps6 = left
+        front = (values[7] + values[0]) * 0.5
+        left  = values[6]
+        right = values[1]
+
+        return {
+            "front": front,
+            "left":  left,
+            "right": right
+        }
